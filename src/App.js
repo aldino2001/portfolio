a12ab54f606c3a5489de28c761524bdb1e7437cc
Aldino2001 <hajaciscoikalamavony@gmail.com>
2024-08-14 14:42:10 +0300

    change desing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn docker 
+          Learn dockers 
         </a>
       </header>
     </div>
