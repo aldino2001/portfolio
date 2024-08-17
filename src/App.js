fcc046f49595a79375290591571a3d96b8b03ea4
Aldino2001 <hajaciscoikalamavony@gmail.com>
2024-08-17 15:40:48 +0300

    backend status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import ContentFile from './Component/ContentFile';
 import './App.css';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
   return (
+    <ThemeProvider theme={darkTheme}>
     <div className="App">
       <ContentFile/>
     </div>
+    </ThemeProvider>
   );
 }
 
